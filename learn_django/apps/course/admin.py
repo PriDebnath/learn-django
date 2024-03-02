6c265df94885904092220f5bcab33e5758be90f6
Pritam <debnathpritam0802@gmail.com>
2024-03-02 20:57:57 +0530

    create course app , added model and registered model in admin

--- learn_django/apps/course/admin.py
+++ learn_django/apps/course/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from learn_django.apps.course.models import Course
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "desc"]
--- learn_django/apps/course/models.py
+++ learn_django/apps/course/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=80)
+    desc = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name  # Returned value will represent the Model
