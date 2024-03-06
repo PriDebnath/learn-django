f3ee7f61206bb750490745a3ea6a20c7ed884452
Pritam <debnathpritam0802@gmail.com>
2024-03-06 18:45:56 +0530

    class based views for course

--- learn_django/apps/course/urls.py
+++ learn_django/apps/course/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # path("get-course-list",views.get_course_list) # simplified version
     path(f"{FUNC_PREFIX}/get-course-list", views.get_course_list),
     path(f"{FUNC_PREFIX}/single-course/<int:course_id>", views.handle_single_course),
+    path(f"{CLASS_PREFIX}/courses", views.CourseListView.as_view()),
+    path(f"{CLASS_PREFIX}/courses/<int:course_id>", views.CourseListView.as_view()),
 ]
--- learn_django/apps/course/views.py
+++ learn_django/apps/course/views.py
@@ -1,13 +1,18 @@
 # Create your views here.
 import json
+from django.views import View
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
-from learn_django.apps.course.models import Course
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+
+# @models
+from learn_django.apps.course.models import Course
 
 #
 # Function based view start
+
 
 @csrf_exempt  # telling Django not to enforce CSRF protection for the view.
 def get_course_list(request):
@@ -37,6 +42,7 @@ def get_course_list(request):
         }
         return JsonResponse(response_data, status=201)
 
+
 @csrf_exempt
 def handle_single_course(request, course_id):
 
@@ -44,9 +50,9 @@ def handle_single_course(request, course_id):
         try:
             course_query_set = Course.objects.get(id=course_id)
             response_data = {
-              "id": course_query_set.id,
-              "title": course_query_set.title,
-              "desc": course_query_set.desc,
+                "id": course_query_set.id,
+                "title": course_query_set.title,
+                "desc": course_query_set.desc,
             }
             return JsonResponse(response_data, safe=False)
         except:
@@ -57,13 +63,15 @@ def handle_single_course(request, course_id):
     if request.method == "PATCH":
         course_query_set = Course.objects.get(id=course_id)
         data = json.loads(request.body)
-        course_query_set.title = data.get("title", course_query_set.title) # dict.get("title", course_query_set.title): This is calling the get method on the dictionary data to retrieve the value associated with the key "title". If the key "title" is present in the data dictionary, the method returns the corresponding value. If the key "title" is not present in the data dictionary, the method returns the default value, which is provided as the second argument. In this case, the default value is course_query_set.title.
+        course_query_set.title = data.get(
+            "title", course_query_set.title
+        )  # dict.get("title", course_query_set.title): This is calling the get method on the dictionary data to retrieve the value associated with the key "title". If the key "title" is present in the data dictionary, the method returns the corresponding value. If the key "title" is not present in the data dictionary, the method returns the default value, which is provided as the second argument. In this case, the default value is course_query_set.title.
         course_query_set.desc = data.get("desc", course_query_set.desc)
         course_query_set.save()
 
         response_data = model_to_dict(course_query_set)
         return JsonResponse(response_data, status=200)
-    
+
     if request.method == "DELETE":
         course_query_set = Course.objects.get(id=course_id)
         course_query_set.delete()
@@ -72,3 +80,73 @@ def handle_single_course(request, course_id):
 
 #
 # Function based view end
+
+
+#
+# Class based view start
+@method_decorator(csrf_exempt, name="dispatch")
+class CourseListView(View):
+
+    def get(self, request, *args, **kwargs):
+        course_id = kwargs.get("course_id")
+        
+        if course_id is not None:
+            # Retrieve details of a specific course
+            course = Course.objects.get(id=course_id)
+            response_data = {
+                "id": course.id,
+                "title": course.title,
+                "desc": course.desc,
+            }
+            return JsonResponse(response_data, safe=False)
+
+        else:
+            # Retrieve a list of all courses
+            course_list = []
+            course_list_query_set = Course.objects.all()
+            for course in course_list_query_set:
+                course_list.append(
+                    {
+                        "id": course.id,
+                        "title": course.title,
+                        "desc": course.desc,
+                    }
+                )
+            return JsonResponse(course_list, safe=False)
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        new_course = Course.objects.create(
+            title=data.get("title"), desc=data.get("desc")
+        )
+        response_data = {
+            "id": new_course.id,
+            "title": new_course.title,
+            "desc": new_course.desc,
+        }
+        return JsonResponse(response_data, status=201)
+
+    def patch(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        course_id = kwargs.get("course_id")
+        course_query_set = Course.objects.get(id=course_id)
+        # Update fields if present in the request payload
+        if "title" in data:
+            course_query_set.title = data["title"]
+        if "desc" in data:
+            course_query_set.desc = data["desc"]
+
+        course_query_set.save()
+
+        response_data = {
+            "id": course_query_set.id,
+            "title": course_query_set.title,
+            "desc": course_query_set.desc,
+        }
+        return JsonResponse(response_data, status=200)
+
+    def delete(self, request, *args, **kwargs):
+        course_id = kwargs.get("course_id")
+        course_query_set = Course.objects.get(id=course_id)
+        course_query_set.delete()
+        return JsonResponse({}, status=204)
