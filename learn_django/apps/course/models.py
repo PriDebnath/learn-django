05d4e51ecf31870d653888b3811066c8fcd19baa
Pritam <debnathpritam0802@gmail.com>
2024-03-06 23:23:56 +0530

    added price field to course

--- learn_django/apps/course/models.py
+++ learn_django/apps/course/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=80)
     desc = models.CharField(max_length=80)
+    price = models.PositiveIntegerField(default=10)
 
     def __str__(self):
         return self.title  # Returned value will represent the Model
