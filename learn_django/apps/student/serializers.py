61a2c335821ade7995a251c7b0bba12976716e18
Pritam <debnathpritam0802@gmail.com>
2024-03-30 20:50:07 +0530

    changes

--- learn_django/apps/student/serializers.py
+++ learn_django/apps/student/serializers.py
@@ -1,15 +1,33 @@
 from rest_framework import serializers
 from learn_django.apps.student.models import Student
+from learn_django.apps.accounts.serializers import UserSerializer
+from django.contrib.auth.models import User 
 
 
 class StudentSerializer(serializers.ModelSerializer):
   student_name = serializers.SerializerMethodField(method_name="get_student_name")
- 
+  user_detail = serializers.SerializerMethodField() # "get_" + field_name become its method_name
+  user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
   class Meta:
     model = Student
-    fields = ["id", "name", "student_name", "age", "current_year", "user"]
+    fields = ["id", "name", "student_name", "age", "current_year", "user", "user_detail"]
   
   def get_student_name(self, obj):
     student_name = f"{obj.user.first_name} {obj.user.last_name}"
     return student_name or "Unknown"
+    
+  def get_user_detail(self, obj):
+    serializedData =  UserSerializer(obj.user)
+    return serializedData.data
+    
+  # Adding Validations
+  def validate(self, data):
+        self.validate_unique_user(data['user'])
+        return data
+
+  def validate_unique_user(self, user):
+        if Student.objects.filter(user=user).exists():
+            raise serializers.ValidationError({ "error": "A student with this user already exists."})
+
     
--- learn_django/apps/student/views.py
+++ learn_django/apps/student/views.py
@@ -8,3 +8,20 @@ from learn_django.apps.student.serializers import StudentSerializer
 class StudentListCreateView(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    
+   
+class StudentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
